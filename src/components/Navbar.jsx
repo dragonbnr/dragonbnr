f83ba4dbd29ff28fd4c7a447871834b36807fb35
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-10-31 02:01:18 -0600

    Add responsive Navbar component with mobile menu and social icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,130 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect, useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
+  const router = useRouter();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div>
-        <nav className="bg-black p-4">
-            <div className="container mx-auto flex justify-between items-center">
-            <Link href="/" className="text-white font-bold text-xl">
-            <Image src="/images/logo.png" width={50} height={50} alt="logo" />
+    <>
+      <div className="w-full h-[78px] flex justify-between items-center px-4 md:px-10 relative max-w-[1440px] min-w-sm mx-auto bg-black">
+        {/* Logo */}
+        <div className="relative z-10 w-[34px] h-[48.86px]">
+          <Image src="/images/logo.png" width={150} height={150} alt="Logo" className="w-[34px] h-[48.86px] object-cover  " />
+        </div>
+
+        {/* Ícono del menú en versión móvil */}
+        <div className="md:hidden">
+          <FaBars
+            className="text-xl cursor-pointer"
+            onClick={toggleMenu}
+            style={{ color: "white" }}
+          />
+        </div>
+
+        {/* Enlaces del centro para versión de escritorio */}
+        <div className="hidden md:flex gap-x-6 font-anton uppercase absolute left-1/2 transform -translate-x-1/2">
+          <Link href="/">Inicio</Link>
+          <Link href="/About">Acerca De</Link>
+          <Link href="/Shop">Tienda</Link>
+        </div>
+
+        {/* Menú móvil */}
+        <div
+          className={`fixed top-0 left-0 w-full h-full flex flex-col justify-center   z-50 p-6 transition-transform transform ${
+            menuOpen ? "translate-x-0 bg-gradient-to-b from-black to-[#CA1B2ED9]" : "-translate-x-full"
+          } overflow-y-auto `}
+        >
+          {/* Botón para cerrar el menú */}
+          <div className="flex justify-end mr-4">
+            <FaTimes
+              className="text-[30px] cursor-pointer text-white "
+              onClick={toggleMenu}
+            />
+          </div>
+
+          {/* Logo */}
+          <div className="w-full flex justify-center ">
+            <Image src="/images/logo.png" width={156} height={161} alt="Logo" />
+          </div>
+
+          {/* Enlaces del menú móvil */}
+          <div className="mt-8 flex flex-col space-y-[15px] text-[26px] text-white text-center ">
+            <Link
+              href="/"
+              onClick={toggleMenu}
+              className={router.pathname === "/" ? "font-bold" : ""}
+            >
+              Inicio
             </Link>
-            <ul className="flex">
-                <li>
-                <Link href="/" className="text-white p-2">Inicio</Link>
-                </li>
-                <li>
-                <Link href="/about" className="text-white p-2">Acerca de</Link>
-                </li>
-                <li>
-                <Link href="/services" className="text-white p-2">Servicios</Link>
-                </li>
-                <li>
-                <Link href="/contact" className="text-white p-2">Contacto</Link>
-                </li>
-            </ul>
+            <Link
+              href="/About"
+              onClick={toggleMenu}
+              className={router.pathname === "/Conocenos" ? "font-bold" : ""}
+            >
+              Conocenos
+            </Link>
+            <Link
+              href="/Shop"
+              onClick={toggleMenu}
+              className={router.pathname === "/Servicios" ? "font-bold" : ""}
+            >
+              Servicios
+            </Link>
+            <Link
+              href="/Shop"
+              onClick={toggleMenu}
+              className={
+                router.pathname === "/Certificaciones" ? "font-bold" : ""
+              }
+            >
+              Certificaciones
+            </Link>
+            <Link
+              href="/Contacto"
+              onClick={toggleMenu}
+              className={router.pathname === "/Contacto" ? "font-bold" : ""}
+            >
+              Contáctanos
+            </Link>
+
+            <div className="w-full flex justify-center items-center">
+              <div className="w-[90%] h-[1px] border-[0.5px] border-white/40" />
             </div>
-        </nav>
-    </div>
-  )
+
+            {/* Íconos de redes sociales */}
+            <div className="flex justify-center space-x-6 mt-8">
+              <Image
+                src={"/images/iconos/fb.png"}
+                width={"34"}
+                height={"34"}
+                className="cursor-pointer"
+              />
+              <Image
+                src={"/images/iconos/instagram.png"}
+                width={"34"}
+                height={"34"}
+                className="cursor-pointer"
+              />
+              <Image
+                src={"/images/iconos/linkedin.png"}
+                width={"34"}
+                height={"34"}
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
