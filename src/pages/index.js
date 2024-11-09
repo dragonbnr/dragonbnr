604e476839a6c7cd81d80bed1f3b7ee82108bbdd
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-11-08 23:06:15 -0600

    Update Open Graph and Twitter meta tags in Home page for improved SEO; adjust image paths for consistency.

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,10 +21,10 @@ export default function Home() {
         <meta name="keywords" content="seguridad privada, vigilancia privada, empresa de seguridad, protección empresarial, guardias de seguridad, monitoreo de seguridad, instalación de cámaras de seguridad, sistemas de videovigilancia, control de acceso, seguridad para empresas, servicio de seguridad 24/7, consultoría en seguridad, sistemas de alarmas, seguridad corporativa, patrullaje y vigilancia, seguridad y protección" />
         <meta property="og:title" content="Dragon Black & Red" />
         <meta property="og:description" content="En Dragon Black & Red, estamos comprometidos con la seguridad integral, ofreciendo servicios de vigilancia privada y tecnología avanzada para proteger empresas, hogares y bienes. Nuestro equipo altamente capacitado utiliza protocolos rigurosos y soluciones de última generación, garantizando una protección constante y confiable que se adapta a las necesidades específicas de cada cliente." />
-        <meta property="og:image" content="/assetsSecurity/ogimage.jpg" />
+        <meta property="og:image" content="/ogimage.jpg" />
         <meta property="og:url" content="https://www.dragonbnr.com" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image" content="/assetsSecurity/ogimage.jpg" />
+        <meta name="twitter:image" content="/ogimage.jpg" />
         <link rel="icon" href="favicon.ico" />
       </Head>
       <HeroSection />
